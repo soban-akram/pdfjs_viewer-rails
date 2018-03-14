4a78f1775aa8de7f4103b6bdc1552881760e38fc
Muhammad Soban Akram <soban.se@gmail.com>
2018-03-14 13:14:42 +0500

    Cors allowed to all

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -6456,10 +6456,11 @@ var pdfjsWebLibs;
      if (HOSTED_VIEWER_ORIGINS.indexOf(viewerOrigin) >= 0) {
       return;
      }
-     var fileOrigin = new URL(file, window.location.href).origin;
-     if (fileOrigin !== viewerOrigin) {
-      throw new Error('file origin does not match viewer\'s');
-     }
+    //  Commented due to seving from S3
+    //  var fileOrigin = new URL(file, window.location.href).origin;
+    //  if (fileOrigin !== viewerOrigin) {
+    //   throw new Error('file origin does not match viewer\'s');
+    //  }
     } catch (e) {
      var message = e && e.message;
      var loadingErrorMessage = mozL10n.get('loading_error', null, 'An error occurred while loading the PDF.');
--- app/controllers/pdfjs_viewer/application_controller.rb
+++ app/controllers/pdfjs_viewer/application_controller.rb
@@ -1,4 +1,11 @@
 module PdfjsViewer
   class ApplicationController < ActionController::Base
+    after_action :allow_iframe
+
+    private
+
+    def allow_iframe
+      response.headers.delete 'X-Frame-Options'
+    end
   end
 end
